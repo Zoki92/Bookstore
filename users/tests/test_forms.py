c17ceaa9a1eaac5770acdd536abf2e398eb09fb3
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-04-02 18:04:13 +0100

    added users creation test for allauth support

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -2,14 +2,25 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from users.forms import CustomUserCreationForm
 from users.views import SignupPageView
+from django.contrib.auth import get_user_model
 
 
 class SignupFormTest(TestCase):
+    username = 'newuser'
+    email = 'newuser@email.com'
+
     def setUp(self):
         url = reverse('signup')
         self.response = self.client.get(url)
 
     def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()
+                         [0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
         form = self.response.context.get('form')
         self.assertIsInstance(form, CustomUserCreationForm)
         self.assertContains(self.response, 'csrfmiddlewaretoken')
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -3,13 +3,14 @@ from django.urls import reverse
 
 
 class SignupPageTests(TestCase):
+
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertTemplateUsed(self.response, 'account/signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
             self.response, 'Hi there! I should not be on this page.')
