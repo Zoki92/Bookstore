81f5eb897ab233a4a95c333a202d90f05cd7254c
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-04-02 17:54:20 +0100

    fixed url name in teplates for allauth

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -149,9 +149,15 @@ LOGIN_REDIRECT_URL = 'home'
 
 
 # Django-Allauth Config
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT = 'home'
 SITE_ID = 1
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
