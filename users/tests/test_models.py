fe5742ce2216ad490794c4a888fbb731b3034696
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-04-02 13:50:58 +0100

    added tests for users

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -16,3 +16,16 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
         self.assertEqual(user.check_password('123testing'), True)
+
+    def test_create_superuser(self):
+        admin_user = get_user_model().objects.create_superuser(
+            username='zoki',
+            password='123testing',
+            email='123@test.com'
+        )
+        self.assertEqual(admin_user.username, 'zoki')
+        self.assertEqual(admin_user.email, '123@test.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        self.assertEqual(admin_user.check_password('123testing'), True)
