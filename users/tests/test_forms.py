7bce799951dbb776a0f0d419077ad8b7dcf3f65d
Zoran <zoranstoilov@yahoo.com>
2019-04-02 18:22:09 +0100

    w

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -10,7 +10,7 @@ class SignupFormTest(TestCase):
     email = 'newuser@email.com'
 
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
 
     def test_signup_form(self):
@@ -21,10 +21,6 @@ class SignupFormTest(TestCase):
         self.assertEqual(get_user_model().objects.all()
                          [0].username, self.username)
         self.assertEqual(get_user_model().objects.all()[0].email, self.email)
-        form = self.response.context.get('form')
-        self.assertIsInstance(form, CustomUserCreationForm)
-        self.assertContains(self.response, 'csrfmiddlewaretoken')
+        
 
-    def test_signup_view(self):
-        view = resolve('/accounts/signup/')
-        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
+   
